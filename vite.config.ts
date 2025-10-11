17386a3491bc19f6991a053063dea39c706d53cb
Grillon <thierry.grillon@gmail.com>
2025-10-11 22:02:16 +0200

    changement de domaine

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/game.streaks.quest/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
